45811b286272affe08ad614887263de2f21a03a4
JohnZGBG <126150193+JohnZGBG@users.noreply.github.com>
2023-09-17 08:57:27 +1000

    edited at 9am on 17.09.2023

---
+++ py4e/chapter5/JohnZ/exercise.py
@@ -0,0 +1,18 @@
+def computepay(c_hours,c_rate):
+
+    if float(c_hours) > 40:
+        penalty_rate_hours = float(c_hours) - 40
+        normal_rate_hours = 40
+        pay = normal_rate_hours * float(c_rate) + penalty_rate_hours * float(c_rate)*1.5     
+
+    else: 
+        pay = float(c_hours) * float(c_rate)
+
+    return(f"Pay {pay}")
+
+hours = input("What is your hours?")
+rate = input("What is your rate?")
+
+final_result = computepay(hours,rate)
+
+print(final_result)
