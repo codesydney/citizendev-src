1d36bf0120baa37a40a3fb12c7846b67b1bc3d0c
JohnZGBG <126150193+JohnZGBG@users.noreply.github.com>
2023-09-16 15:00:18 +1000

    edited at 3pm on 16.09.2023

--- py4e/chapter4/JohnZ/Exercise2.py
+++ py4e/chapter4/JohnZ/Exercise2.py
@@ -1,7 +1,15 @@
 input_number = input("Please enter a number between 0.0 and 1.0: ")
 
 
-number = float(input_number)
+
+
+try: 
+    number = float(input_number)
+
+except:
+    print("Please enter a number")
+    quit()
+
 
 if number >= 0 and number < 0.6:
     final_number = "F"
@@ -13,7 +21,8 @@ elif number >= 0.8 and number <0.9:
     final_number = "B"
 elif number>= 0.9 and number <=1:
     final_number = "A"
-
+else:
+    final_number = "Please enter a number between 0 and 1."
 
 print(final_number)
 
