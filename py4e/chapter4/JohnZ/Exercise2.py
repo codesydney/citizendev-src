18bf15b5460098362bbcc2c3d762429a4f481e36
JohnZGBG <126150193+JohnZGBG@users.noreply.github.com>
2023-09-16 14:12:54 +1000

    edited on 16.09.2023

--- py4e/chapter4/JohnZ/Exercise2.py
+++ py4e/chapter4/JohnZ/Exercise2.py
@@ -5,13 +5,13 @@ number = float(input_number)
 
 if number >= 0 and number < 0.6:
     final_number = "F"
-if number>= 0.6 and number < 0.7:
+elif number>= 0.6 and number < 0.7:
     final_number = "D"
 elif number >= 0.7 and number <0.8:
     final_number = "C"
 elif number >= 0.8 and number <0.9:
     final_number = "B"
-elif number>= 0.9 and number <1:
+elif number>= 0.9 and number <=1:
     final_number = "A"
 
 
