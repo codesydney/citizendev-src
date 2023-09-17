120e8164f750a02b693c5fa931e725af700c0d00
Tony Johnson <tony@tosp.net.au>
2023-09-17 11:01:13 +1000

    Added exercise 3.3

---
+++ py4e/chapter4/tony/exercise3.3.py
@@ -0,0 +1,20 @@
+score = input("Enter Score: ")
+
+try:
+    score = float(score)
+
+    if score > 1.0 or score < 0.0:
+        print("Score must be between 0.0 and 1.0")
+    elif score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
+    else:
+        print("F")
+
+except:
+    print("Did not enter a valid number")
