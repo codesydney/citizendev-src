227b1395a38ac07ade6391cfbd37ee4668fe3b47
Engramar Bollas <engramar.bollas@gmail.com>
2023-09-22 05:10:03 +1000

    Adjusted chapter numbering

--- py4e/chapter3/JohnZ/Hello_Sarah.py
+++
@@ -1,3 +0,0 @@
-name=input("What is your name?")
-print(f"My name is {name}")
-
--- py4e/chapter3/JohnZ/PayCalculate.py
+++
@@ -1,4 +0,0 @@
-hours = input("What is your hours?")
-rate = input("What is your rate?")
-pay = float(hours) * float(rate)
-print(f"This is your {pay}")
