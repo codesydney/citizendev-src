9eba3da5d817fa043efeb5ad48d8b48d09dd4871
Tony Johnson <tony@tosp.net.au>
2023-09-25 09:29:40 +1000

    Added exercise7.2

---
+++ py4e/chapter7/tony/exercise7.2.py
@@ -0,0 +1,30 @@
+def spam_confidence(line):
+    i = line.find(":") + 1
+    return float(line[i:])
+
+
+CHECK = "X-DSPAM-Confidence:"
+
+while True:
+    try:
+        fname = input("Enter file name: ")
+        fh = open(fname)
+
+        count = 0
+        tot = 0.0
+
+        for line in fh:
+            if not line.startswith(CHECK):
+                continue
+            count += 1
+            tot += spam_confidence(line)
+        print("Average spam confidence:", tot / count)
+        exit(0)
+
+    except FileNotFoundError:
+        print("That file does not exist. Try again.")
+        continue
+
+    except ValueError:
+        print("Cannot convert string to float")
+        exit(1)
