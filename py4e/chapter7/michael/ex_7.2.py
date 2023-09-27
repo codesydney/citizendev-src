0df1bf8486a051942db69a96a75bb8ad96bacae9
starkskyy <michael.p_92@hotmail.com>
2023-09-28 01:40:09 +1000

    cleaning comments

--- py4e/chapter7/michael/ex_7.2.py
+++ py4e/chapter7/michael/ex_7.2.py
@@ -13,13 +13,13 @@ for line in fh:
 
     #increase running count
     count = count + 1
-    
-    #increase running sum
-    ##finding float value
+
+    #finding float value
     strlength = len(str_of_int)
     spos = line.find(str_of_int)
     s_conf = float (line[spos + strlength:])
-    ##sum of float values
+
+    ##running sum of float values
     total_s_conf = total_s_conf + s_conf
 
 avg_s_conf = total_s_conf/count
