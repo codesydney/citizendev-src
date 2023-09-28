00fa5377025bd5cb062196694b564299c1768c28
Sadruddin Junejo <sjunejo1@gmail.com>
2023-09-28 18:35:54 +1000

    Sunny add exercise7.2 solution

---
+++ py4e/chapter7/sunny/exercise7.2.py
@@ -0,0 +1,24 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+except:
+    print("Invalid file name, please check your input and try again.")
+    exit(0)
+
+number_of_floating_point_values = 0
+total_of_floating_point_values = 0.0
+
+
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    floating_point_value = float(line[line.find(' '):])
+    number_of_floating_point_values = number_of_floating_point_values + 1
+    total_of_floating_point_values = total_of_floating_point_values + floating_point_value
+
+fh.close()
+
+average_of_floating_point_values = total_of_floating_point_values / number_of_floating_point_values
+
+print(f"Average spam confidence: {average_of_floating_point_values}")
