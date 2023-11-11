1adde47a976635a214bc124e74df72d686442f6c
Hossein Roshandel <hossein.roshandel@gmail.com>
2023-11-11 20:19:42 +1100

    Add variables exercises by Hossein

---
+++ py4e/chapter02/hossein/exercise2.3.py
@@ -0,0 +1,5 @@
+hours = int(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+pay = round(hours*rate, 2)
+
+print(f"Pay: {pay}")
---
+++ py4e/chapter02/hossein/exercise2.5.py
@@ -0,0 +1,4 @@
+celsius_temp = float(input("Enter a temperature in Celsius: "))
+fahrenheit_temp = celsius_temp*1.8+32
+
+print(f"The converted temperature in Fahrenheit: {round(fahrenheit_temp,2)}")
